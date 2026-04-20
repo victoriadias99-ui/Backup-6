0bb26b6b0dc61b5b88071fd659823ba7cc6b1185
victoriadias99-ui <victoriadias99@outlook.com>
2026-04-20 17:12:19 -0300

    Change image class for mobile and desktop views

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -90,7 +90,7 @@ export default function Hero() {
               <img
                 src={slide.image}
                 alt={slide.title}
-                className={`w-full h-full object-contain hidden md:block`}
+                className={`w-full h-full object-cover ${slide.position} block md:hidden`}
                 referrerPolicy="no-referrer"
                 decoding="async"
                 loading={index === 0 ? 'eager' : 'lazy'}
